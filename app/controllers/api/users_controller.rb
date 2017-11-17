da058c247512d3124fad6fa9c5ea8537c35fb082
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-17 10:52:02 -0800

    Adds logic to users_controller and sessions_controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,22 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      # redirect_to
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      # render :new
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
