6c2114c7b16bced3889e6f764165156d64e73ee4
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-17 11:50:57 -0800

    Adds actions for session in frontend folder

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,46 @@
+import * as APIUtil from '../util/session_api_util'
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const RECEIVE_LOGOUT_SUCCESS = "RECEIVE_LOGOUT_SUCCESS";
+
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const SIGNUP = 'SIGNUP';
+
+// ASYNC ACTIONS
+export const requestSignup = user => dispatch => {
+  return APIUtil.signup(user).then(
+    currentUser => dispatch(receiveCurrentUser(currentUser)),
+    error => dispatch(receiveErrors(error.responseJSON))
+  );
+};
+
+export const requestLogin = user => dispatch => {
+  return APIUtil.login(user).then(
+    currentUser => dispatch(receiveCurrentUser(currentUser)),
+    error = > dispatch(receiveErrors(error.responseJSON))
+  );
+};
+
+export const requestLogout = () => dispatch => {
+  return APIUtil.logout().then(
+    () => dispatch(receiveLogoutSuccess())
+  );
+};
+
+
+// // SYNC ACTIONS
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER
+  currentUser
+});
+
+export const receiveLogoutSuccess = () => ({
+  type: RECEIVE_LOGOUT_SUCCESS
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS
+  errors
+});
