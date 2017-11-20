9ed4cd207775fd1c62907441ad6a531663969452
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-20 09:31:29 -0800

    Seeds the database with few locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bench.delete_all
+
+Bench.create!(
+  description: 'Ferry Builidng',
+  lat: 37.795270,
+  lng: -122.393463,
+)
+
+Bench.create!(
+  description: 'Golden Gate Bridge',
+  lat: 37.814243,
+  lng: -122.477616,
+)
+
+Bench.create!(
+  description: 'Hyatt',
+  lat: 37.794101,
+  lng: -122.395897,
+)
